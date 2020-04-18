78565c31683662f3b844310ae18d48edaaead15e
Aji Saputra Raka Siwi <ajisaputrars@gmail.com>
2020-04-18 14:33:44 +0700

    Initial Commit

---
+++ a171-ios-pemula-submissionTests/a171_ios_pemula_submissionTests.swift
@@ -0,0 +1,34 @@
+//
+//  a171_ios_pemula_submissionTests.swift
+//  a171-ios-pemula-submissionTests
+//
+//  Created by Aji Saputra Raka Siwi on 18/04/20.
+//  Copyright © 2020 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import XCTest
+@testable import a171_ios_pemula_submission
+
+class a171_ios_pemula_submissionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
